9a8fb7e8825c08dac757572657560b8f7de884c2
Manukumar N M <manunm.jps@gmail.com>
2021-11-05 13:14:19 +0530

    Update settings.py

--- Advisor_Network/settings.py
+++ Advisor_Network/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%1l&3yidy!2cg144-m0-j_u+bmghie_+nb!@yrleaavq=o!*ke'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000', 'advisor-api-heroku.herokuapp.com' ]
 
 
 # Application definition
